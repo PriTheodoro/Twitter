7087ad9302529243344a328e8662e061738a6532
PriTheodoro <pri.paf@gmail.com>
2019-02-21 14:27:30 -0300

    js com todas as versoes

--- twitter.js
+++ twitter.js
@@ -1,30 +1,57 @@
 let btnInsert = document.getElementById("btn");
-btnInsert.addEventListener('click', ()=>{
-    insert();
-});
-
+btnInsert.addEventListener('click', insert);
+btnInsert.addEventListener('mouseover', btnNone)
 
 
 function insert() {
     let getText = document.getElementById("insertText").value;
-
+    
     document.getElementById("insertText").value = "";
     
     let printDateHour = document.getElementById('talks');
     let newDate = document.createElement('p');
-
+    
     printDateHour.insertBefore(newDate, printDateHour.firstChild);
     newDate.innerHTML = momentTime();
-
+    
     let twittes = document.getElementById('talks');
     let newText = document.createElement('p');
     
     twittes.insertBefore(newText, twittes.firstChild);
-    newText.innerHTML = getText
-    
-
-  
+    newText.innerHTML = getText 
 }
+
+function btnNone (){ 
+    let textNone = document.getElementById("insertText").value;
+    
+    if (textNone.length === 0 || textNone.length > 140){
+        document.getElementById("btn").disabled = true
+    } else  {
+        document.getElementById("btn").disabled = false
+    }
+}
+
+document.getElementById("insertText").addEventListener('keyup', counter);    
+
+function counter(){
+    let textMax = 140;
+    let contText = document.getElementById("insertText").value.length;
+    let reduce = textMax - contText;
+    let contStyle = document.getElementById('counter');
+    document.getElementById('counter').innerHTML = reduce;
+
+
+    if (contText >= 120) {
+        contStyle.style.color = 'orange';
+
+    } if (contText >= 130){
+        contStyle.style.color = 'red';
+
+    } if (contText < 120){
+        contStyle.style.color = 'black';
+    }
+}
+counter();
 
 function momentTime(){
     let date = new Date;
@@ -33,5 +60,13 @@ function momentTime(){
     let finalDateHour = hour +":"+ minutes;
 
     return finalDateHour
-
 }
+
+document.getElementById("insertText").addEventListener('keydown', textSize)
+
+function textSize (){
+    let messageArea = document.getElementById("insertText");
+    messageArea.style.height = 'auto';
+    messageArea.style.height = messageArea.scrollHeight + 'px';
+}
+
