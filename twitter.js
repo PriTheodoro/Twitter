47eb23a08bbbb088124ee640682ca88f75c8868e
PriTheodoro <pri.paf@gmail.com>
2019-02-21 11:39:33 -0300

    primeiro commit com data e hora

--- twitter.js
+++ twitter.js
@@ -1,11 +1,37 @@
+let btnInsert = document.getElementById("btn");
+btnInsert.addEventListener('click', ()=>{
+    insert();
+});
+
+
+
 function insert() {
     let getText = document.getElementById("insertText").value;
 
     document.getElementById("insertText").value = "";
+    
+    let printDateHour = document.getElementById('talks');
+    let newDate = document.createElement('p');
 
-    let newList = document.createElement('p');
-    let printList = document.createTextNode(getText);
-    newList.appendChild(printList)
-    document.body.appendChild(newList);
+    printDateHour.insertBefore(newDate, printDateHour.firstChild);
+    newDate.innerHTML = momentTime();
+
+    let twittes = document.getElementById('talks');
+    let newText = document.createElement('p');
+    
+    twittes.insertBefore(newText, twittes.firstChild);
+    newText.innerHTML = getText
+    
+
+  
+}
+
+function momentTime(){
+    let date = new Date;
+    let hour = date.getHours().toString();
+    let minutes = date.getMinutes().toString();
+    let finalDateHour = hour +":"+ minutes;
+
+    return finalDateHour
 
 }
